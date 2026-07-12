dce627d939832ad895a0b9782c037ca2956a9772
nishchay-snek <108711354+NishchayKQ@users.noreply.github.com>
2026-07-12 15:15:47 +0530

    adding notification channels to SalamanderApplication class

--- app/src/main/java/nish/wry/salamander/di/SalamanderApplication.kt
+++ app/src/main/java/nish/wry/salamander/di/SalamanderApplication.kt
@@ -1,15 +1,49 @@
 package nish.wry.salamander.di
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import kotlinx.coroutines.MainScope
+import nish.wry.salamander.data.Priority
+
 
 class SalamanderApplication : Application() {
     lateinit var container: AppContainer
 
     override fun onCreate() {
         super.onCreate()
+
+        // create notification channels our app will use
+        createNotificationChannel()
+
         // TODO check if this scope is okay
         container = AppDataContainer(coroutineScope = MainScope(), context = this)
     }
 
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val notificationManager: NotificationManager =
+                getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            // notification channel for tasks of all priorities
+            Priority.entries.forEach { priority ->
+                val name = priority.nameSeenToUser
+                val descriptionText = priority.description
+                val importance = priority.importance
+
+                val channel = NotificationChannel(priority.name, name, importance).apply {
+                    description = descriptionText
+                }
+
+                // Register the channel with the system.
+
+                notificationManager.createNotificationChannel(channel)
+
+            }
+
+        }
+    }
 }
